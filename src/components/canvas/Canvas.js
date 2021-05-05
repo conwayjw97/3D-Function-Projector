586836a2b8c392d11eaef912f1d2cbae65d242f2
James <conwayjw97@gmail.com>
2021-05-05 20:52:53 +0100

    CanvasDrawer refactoring finished

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -2,79 +2,8 @@
 // TODO: Create camera class to encapsulate rotational data?
 
 import React, { useEffect, useRef } from "react";
-
-import Polygon from "../../utils/modelling/Polygon.js"
-import Vertex from "../../utils/modelling/Vertex.js"
-import Mat3 from "../../utils/math/Mat3.js"
-import Vec3 from "../../utils/math/Vec3.js"
-
+import CanvasDrawer from "./utils/CanvasDrawer.js"
 import "./Canvas.css";
-
-const vertices = [
-  new Vertex(-1.0, -1.0, -1.0), // Front-Bottom-Left
-  new Vertex( 1.0, -1.0, -1.0), // Front-Bottom-Right
-  new Vertex(-1.0, -1.0,  1.0), // Rear-Bottom-Left
-  new Vertex( 1.0, -1.0,  1.0), // Rear-Bottom-Right
-  new Vertex(-1.0,  1.0, -1.0), // Front-Top-Left
-  new Vertex( 1.0,  1.0, -1.0), // Front-Top-Right
-  new Vertex(-1.0,  1.0,  1.0), // Rear-Top-Left
-  new Vertex( 1.0,  1.0,  1.0)  // Rear-Top-Right
-]
-
-const polygons = [
-  new Polygon([vertices[0], vertices[1], vertices[5], vertices[4]]), // Front
-  new Polygon([vertices[2], vertices[3], vertices[7], vertices[6]]), // Rear
-  new Polygon([vertices[0], vertices[1], vertices[3], vertices[2]]), // Bottom
-  new Polygon([vertices[4], vertices[5], vertices[7], vertices[6]]), // Top
-  new Polygon([vertices[0], vertices[2], vertices[6], vertices[4]]), // Left
-  new Polygon([vertices[1], vertices[3], vertices[7], vertices[5]]), // Right
-]
-
-function drawLineFromVectors(ctx, a, b) {
-	ctx.beginPath();
-	ctx.moveTo(a.element(0), -1 * a.element(1));
-	ctx.lineTo(b.element(0), -1 * b.element(1));
-	ctx.stroke();
-};
-
-function drawLineBetweenVertices(ctx, a, b, matrix, fx, fy) {
-  a = a.transform(matrix);
-  b = b.transform(matrix);
-
-  ctx.beginPath();
-  ctx.moveTo(fx(a), -1 * fy(a));
-  ctx.lineTo(fx(b), -1 * fy(b));
-  ctx.closePath();
-  ctx.stroke();
-};
-
-function drawPolygon(ctx, polygon, matrix, fx, fy){
-  ctx.beginPath();
-
-  // Get the first vertex and apply the transformation matrix
-  let vertex = polygon.vertex(0).transform(matrix);
-
-  // Move to first vertexes coordinates on canvas using canvas positioning functions
-  // The -1 is used to flip the y coordinate as y increases as you move down the canvas
-  ctx.moveTo(fx(vertex), -1 * fy(vertex));
-  // For each vertex, draw a line to that vertexes coordinates
-  for(let i=1; i<polygon.count(); i++){
-    vertex = polygon.vertex(i).transform(matrix);
-    ctx.lineTo(fx(vertex), -1 * fy(vertex));
-  }
-  ctx.closePath();
-  ctx.stroke();
-}
-
-function drawAxisIndicator(ctx, matrix, fx, fy){
-	ctx.save();
-
-  drawLineBetweenVertices(ctx, new Vertex(-1.0, 0, 0), new Vertex(1.0, 0, 0), matrix, fx, fy);
-  drawLineBetweenVertices(ctx, new Vertex(0, -1.0, 0), new Vertex(0, 1.0, 0), matrix, fx, fy);
-	drawLineBetweenVertices(ctx, new Vertex(0, 0, -1.0), new Vertex(0, 0, 1.0), matrix, fx, fy);
-
-	ctx.restore();
-}
 
 function Canvas(props) {
   const canvas = useRef(null);
@@ -83,39 +12,8 @@ function Canvas(props) {
 
   useEffect(() => {
     const ctx = canvas.current.getContext("2d");
-    // Make 0 the centre value of the canvas context
-    ctx.translate(width/2, height/2);
-	  ctx.strokeStyle = "rgb(255, 255, 255)";
-
-    let size = height;          // Size of the contents
-    let cx = 0.000, cy = 0.000; // Rotational movement
-    let lx = null, ly = null;   // Last set rotation position
-    let x = 0, y = 0;           // Current rotation position
-
-    const render = () => {
-      // Update canvas rotation
-      x += cx;
-    	y += cy;
-
-      // Clear canvas contents
-      ctx.clearRect(-width/2, -height/2, width, height);
-
-      // Create transformation matrix to determine how the object's coordinates
-      // move with respect to the rotation
-      const transform = Mat3.rotationX(-y * 2 * Math.PI).multiply(Mat3.rotationY(-x * 2 * Math.PI));
-
-      // Canvas positioning functions
-      const fx = (vertex) => vertex.x * size;
-      const fy = (vertex) => vertex.y * size;
-
-      drawAxisIndicator(ctx, transform, fx, fy);
-
-      // for(let i=0; i<polygons.length; i++){
-      //   drawPolygon(ctx, polygons[i], transform, fx, fy);
-      // }
-    }
-
-    render();
+    const drawer = new CanvasDrawer(ctx, width, height, height);
+    drawer.render();
 
     let mouseDown = false;
 
@@ -125,29 +23,16 @@ function Canvas(props) {
 
     canvas.current.onmousemove = (e) => {
       if(mouseDown){
-        // User mouse down coordinates
-        const mouseX = e.clientX;
-        const mouseY = e.clientY;
         // New position for canvas contents
-    		const px = mouseX / width;
-    		const py = mouseY / height;
-        // If last rotational position was set, update rotational movement values
-    		if (lx && ly) {
-    			cx = (px - lx);
-    			cy = (py - ly);
-    		}
-        // Update last rotational position to the position observed here
-    		lx = px;
-    		ly = py;
-        render();
+    		const px = e.clientX / width;
+    		const py = e.clientY / height;
+        drawer.onMouseMove(px, py);
       }
     }
 
     canvas.current.onmouseup = (e) => {
       mouseDown = false;
-      // Nullify last rotational position so next mouse down doesn't update movement values
-  		lx = null;
-  		ly = null;
+      drawer.onMouseUp();
     }
 
     canvas.current.onwheel = (e) => {
--- src/components/canvas/utils/CanvasDrawer.js
+++ src/components/canvas/utils/CanvasDrawer.js
@@ -1,15 +1,53 @@
+import Polygon from "../../../utils/modelling/Polygon.js"
+import Vertex from "../../../utils/modelling/Vertex.js"
+import Mat3 from "../../../utils/math/Mat3.js"
+import Vec3 from "../../../utils/math/Vec3.js"
+
+const white = "rgb(255, 255, 255)";
+const green = "rgb(36, 173, 48)";
+const blue = "rgb(43, 125, 207)";
+const red = "rgb(207, 43, 43)";
+
+const vertices = [
+  new Vertex(-1.0, -1.0, -1.0), // Front-Bottom-Left
+  new Vertex( 1.0, -1.0, -1.0), // Front-Bottom-Right
+  new Vertex(-1.0, -1.0,  1.0), // Rear-Bottom-Left
+  new Vertex( 1.0, -1.0,  1.0), // Rear-Bottom-Right
+  new Vertex(-1.0,  1.0, -1.0), // Front-Top-Left
+  new Vertex( 1.0,  1.0, -1.0), // Front-Top-Right
+  new Vertex(-1.0,  1.0,  1.0), // Rear-Top-Left
+  new Vertex( 1.0,  1.0,  1.0)  // Rear-Top-Right
+]
+
+const polygons = [
+  new Polygon([vertices[0], vertices[1], vertices[5], vertices[4]]), // Front
+  new Polygon([vertices[2], vertices[3], vertices[7], vertices[6]]), // Rear
+  new Polygon([vertices[0], vertices[1], vertices[3], vertices[2]]), // Bottom
+  new Polygon([vertices[4], vertices[5], vertices[7], vertices[6]]), // Top
+  new Polygon([vertices[0], vertices[2], vertices[6], vertices[4]]), // Left
+  new Polygon([vertices[1], vertices[3], vertices[7], vertices[5]]), // Right
+]
+
 export default class CanvasDrawer {
-  constructor(ctx, size) {
+  constructor(ctx, width, height, size) {
     this.ctx = ctx;
+    this.width = width;
+    this.height = height;
     this.size = size;
 
     // Make 0 the centre value of the canvas context
     this.ctx.translate(width/2, height/2);
     this.ctx.strokeStyle = "rgb(255, 255, 255)";
 
-    this.cx = 0.000, this.cy = 0.000; // Rotational movement
-    this.lx = null, this.ly = null;   // Last set rotation position
-    this.x = 0, this.y = 0;           // Current rotation position
+    // Rotational movement
+    this.cx = 0.000
+    this.cy = 0.000;
+    // Last set rotation position
+    this.lx = null
+    this.ly = null;
+    // Current rotation position
+    this.x = 0
+    this.y = 0;
 
     // Canvas positioning functions
     this.fx = (vertex) => vertex.x * size;
@@ -41,10 +79,10 @@ export default class CanvasDrawer {
     b = b.transform(matrix);
 
     this.ctx.beginPath();
-    ctx.moveTo(fx(a), -1 * fy(a));
-    ctx.lineTo(fx(b), -1 * fy(b));
-    ctx.closePath();
-    ctx.stroke();
+    this.ctx.moveTo(this.fx(a), -1 * this.fy(a));
+    this.ctx.lineTo(this.fx(b), -1 * this.fy(b));
+    this.ctx.closePath();
+    this.ctx.stroke();
   };
 
   drawPolygon(polygon, matrix){
@@ -65,19 +103,32 @@ export default class CanvasDrawer {
     this.ctx.stroke();
   }
 
+  drawAxisIndicator(matrix){
+  	this.ctx.save();
+
+    this.ctx.strokeStyle = red;
+    this.drawLineBetweenVertices(new Vertex(-1.0, 0, 0), new Vertex(1.0, 0, 0), matrix);
+    this.ctx.strokeStyle = green;
+    this.drawLineBetweenVertices(new Vertex(0, -1.0, 0), new Vertex(0, 1.0, 0), matrix);
+    this.ctx.strokeStyle = blue;
+  	this.drawLineBetweenVertices(new Vertex(0, 0, -1.0), new Vertex(0, 0, 1.0), matrix);
+
+  	this.ctx.restore();
+  }
+
   render(){
     // Update canvas rotation
     this.x += this.cx;
     this.y += this.cy;
 
     // Clear canvas contents
-    this.ctx.clearRect(-width/2, -height/2, width, height);
+    this.ctx.clearRect(-this.width/2, -this.height/2, this.width, this.height);
 
     // Create transformation matrix to determine how the object's coordinates
     // move with respect to the rotation
-    const transform = Mat3.rotationX(-y * 2 * Math.PI).multiply(Mat3.rotationY(-x * 2 * Math.PI));
+    const transform = Mat3.rotationX(-this.y * 2 * Math.PI).multiply(Mat3.rotationY(-this.x * 2 * Math.PI));
 
-    drawAxisIndicator(transform);
+    this.drawAxisIndicator(transform);
 
     // for(let i=0; i<polygons.length; i++){
     //   drawPolygon(ctx, polygons[i], transform, fx, fy);
