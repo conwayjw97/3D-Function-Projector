b51fbd57545a9ac009e5ff7e56a18b124995d39d
James <conwayjw97@gmail.com>
2021-05-07 16:41:07 +0100

    Line and plane drawing code now working (WILL destroy your GPU)

--- src/components/canvas/utils/Graphics.js
+++ src/components/canvas/utils/Graphics.js
@@ -24,7 +24,7 @@ export default class Graphics{
     controls.target = new THREE.Vector3(0, 0, 0);
 
     this.renderAxisIndicators();
-    this.renderExpression("x^2");
+    this.renderExpression("1-abs(x+y)-abs(y-x)");
 
     const animate = () => {
     	requestAnimationFrame(animate);
@@ -58,10 +58,11 @@ export default class Graphics{
 
   renderExpression(expression){
     // Evaluate expression points
-    let points = [];
+    let xPoints = [];
     if(expression.includes("x")){
       for(let x=-100; x<100; x+=1){
         let xEval = expression.replaceAll("x", x);
+        let yPoints = [];
         for(let y=-100; y<100; y+=1){
           let zEval;
           if(xEval.includes("y")){
@@ -72,38 +73,44 @@ export default class Graphics{
             zEval = evaluate(xEval);
           }
           if(zEval>=-100 && zEval<=100){
-            points.push(new THREE.Vector3(x, zEval, y));
+            yPoints.push(new THREE.Vector3(x, zEval, y));
           }
+        }
+        if(yPoints.length > 0){
+          xPoints.push(yPoints);
         }
       }
     }
 
     // Render point planes
-    for(let x=0; x<points.length-201; x+=1){
-      const lineMaterial = new THREE.LineBasicMaterial({color: white});
-      const linePoints = [points[x], points[x+1], points[x+200], points[x+201]];
-      const lineGeometry = new THREE.BufferGeometry().setFromPoints(linePoints);
-      const line = new THREE.Line(lineGeometry, lineMaterial);
+    for(let x=0; x<xPoints.length-1; x+=1){
+      for(let y=0; y<xPoints[x].length-1; y+=1){
+        // const lineMaterial = new THREE.LineBasicMaterial({color: white});
+        // const linePoints = [xPoints[x][y], xPoints[x+1][y], xPoints[x+1][y+1], xPoints[x][y+1], xPoints[x][y]];
+        // const lineGeometry = new THREE.BufferGeometry().setFromPoints(linePoints);
+        // const line = new THREE.Line(lineGeometry, lineMaterial);
+        //
+        // this.scene.add(line);
 
-      this.scene.add(line);
+        let planePointsA = [xPoints[x][y], xPoints[x+1][y], xPoints[x+1][y+1]];
+        let planeGeometryA = new THREE.BufferGeometry().setFromPoints(planePointsA);
+        planeGeometryA.computeVertexNormals();
 
-      // let planePointsA = [points[x], points[x+1], points[x+200]];
-      // let planeGeometryA = new THREE.BufferGeometry().setFromPoints(planePointsA);
-      // planeGeometryA.computeVertexNormals();
-      //
-      // let planePointsB = [points[x+1], points[x+200], points[x+201]];
-      // let planeGeometryB = new THREE.BufferGeometry().setFromPoints(planePointsB);
-      // planeGeometryB.computeVertexNormals();
-      //
-      // const material = new THREE.MeshBasicMaterial( {color: red, side: THREE.DoubleSide} );
-      // this.scene.add(new THREE.Mesh( planeGeometryA, material ));
-      // this.scene.add(new THREE.Mesh( planeGeometryB, material ));
+        let planePointsB = [xPoints[x][y], xPoints[x][y+1], xPoints[x+1][y+1]];
+        let planeGeometryB = new THREE.BufferGeometry().setFromPoints(planePointsB);
+        planeGeometryB.computeVertexNormals();
+
+        const material = new THREE.MeshBasicMaterial( {color: red, side: THREE.DoubleSide} );
+        this.scene.add(new THREE.Mesh( planeGeometryA, material ));
+        this.scene.add(new THREE.Mesh( planeGeometryB, material ));
+      }
     }
 
     // Render points
-    const geometry = new THREE.BufferGeometry().setFromPoints(points);
-    const material = new THREE.PointCloudMaterial({color: white, size: 0.25});
-    const expressionPoints = new THREE.Points(geometry, material);
-    this.scene.add(expressionPoints);
+    // const points = [].concat.apply([], xPoints);
+    // const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    // const material = new THREE.PointCloudMaterial({color: white, size: 0.25});
+    // const expressionPoints = new THREE.Points(geometry, material);
+    // this.scene.add(expressionPoints);
   }
 }
