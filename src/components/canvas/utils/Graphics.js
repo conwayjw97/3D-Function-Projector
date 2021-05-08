2dfff5ea1ca3e64e1f78442e2d67b1d5c428db28
James <conwayjw97@gmail.com>
2021-05-08 15:41:06 +0100

    Fixed evaluation and added graph size projection scaling

--- src/components/canvas/utils/Graphics.js
+++ src/components/canvas/utils/Graphics.js
@@ -11,10 +11,12 @@ const blue = "rgb(43, 125, 207)";
 const red = "rgb(207, 43, 43)";
 
 export default class Graphics{
-  constructor(canvas, width, height, detail, xRange, yRange){
+  constructor(canvas, width, height, expression, detail, xRange, yRange, zRange){
+    this.expression = expression;
     this.detail = detail;
     this.xRange = xRange;
     this.yRange = yRange;
+    this.zRange = zRange;
 
     this.scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(45, width/height, 1, 500);
@@ -28,7 +30,7 @@ export default class Graphics{
     controls.target = new THREE.Vector3(0, 0, 0);
 
     this.renderAxisIndicators();
-    this.renderExpression("1-abs(x+y)-abs(y-x)");
+    this.renderExpression();
 
     const animate = () => {
     	requestAnimationFrame(animate);
@@ -61,60 +63,76 @@ export default class Graphics{
   }
 
   renderExpression(expression){
-    // Evaluate expression points
-    let xPoints = [];
-    if(expression.includes("x")){
+    const expPoints = this.evaluateExpression();
+
+    this.renderExpressionDots(expPoints);
+
+    for(let x=0; x<expPoints.length-1; x+=1){
+      for(let y=0; y<expPoints[x].length-1; y+=1){
+        this.renderExpressionSquare(expPoints, x, y);
+        this.renderExpressionPlane(expPoints, x, y);
+      }
+    }
+  }
+
+  evaluateExpression(){
+    const xScale = 100/this.xRange[1];
+    const yScale = 100/this.yRange[1];
+    const zScale = 100/this.zRange[1];
+    let expPoints = [];
+    if(this.expression.includes("x")){
       for(let x=this.xRange[0]; x<this.xRange[1]; x+=this.detail){
-        let xEval = expression.replaceAll("x", x);
+        let xEval = this.expression.replaceAll("x", "("+x+")");
         let yPoints = [];
         for(let y=this.yRange[0]; y<this.yRange[1]; y+=this.detail){
           let zEval;
           if(xEval.includes("y")){
-            let yEval = xEval.replaceAll("y", y);
+            let yEval = xEval.replaceAll("y", "("+y+")");
             zEval = evaluate(yEval);
           }
           else{
             zEval = evaluate(xEval);
           }
-          if(zEval>=-100 && zEval<=100){
-            yPoints.push(new THREE.Vector3(x, zEval, y));
+          if(zEval>=this.zRange[0] && zEval<=this.zRange[1]){
+            yPoints.push(new THREE.Vector3(x*xScale, zEval*zScale, y*yScale));
           }
         }
         if(yPoints.length > 0){
-          xPoints.push(yPoints);
+          expPoints.push(yPoints);
         }
       }
     }
+    return expPoints;
+  }
 
-    // Render point planes
-    for(let x=0; x<xPoints.length-1; x+=1){
-      for(let y=0; y<xPoints[x].length-1; y+=1){
-        const lineMaterial = new THREE.LineBasicMaterial({color: white});
-        const linePoints = [xPoints[x][y], xPoints[x+1][y], xPoints[x+1][y+1], xPoints[x][y+1], xPoints[x][y]];
-        const lineGeometry = new THREE.BufferGeometry().setFromPoints(linePoints);
-        const line = new THREE.Line(lineGeometry, lineMaterial);
+  renderExpressionDots(expPoints){
+    const points = [].concat.apply([], expPoints);
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const material = new THREE.PointCloudMaterial({color: white, size: 0.25});
+    const expressionPoints = new THREE.Points(geometry, material);
+    this.scene.add(expressionPoints);
+  }
 
-        this.scene.add(line);
+  renderExpressionSquare(expPoints, x, y){
+    const lineMaterial = new THREE.LineBasicMaterial({color: white});
+    const linePoints = [expPoints[x][y], expPoints[x+1][y], expPoints[x+1][y+1], expPoints[x][y+1], expPoints[x][y]];
+    const lineGeometry = new THREE.BufferGeometry().setFromPoints(linePoints);
+    const line = new THREE.Line(lineGeometry, lineMaterial);
 
-        // let planePointsA = [xPoints[x][y], xPoints[x+1][y], xPoints[x+1][y+1]];
-        // let planeGeometryA = new THREE.BufferGeometry().setFromPoints(planePointsA);
-        // planeGeometryA.computeVertexNormals();
-        //
-        // let planePointsB = [xPoints[x][y], xPoints[x][y+1], xPoints[x+1][y+1]];
-        // let planeGeometryB = new THREE.BufferGeometry().setFromPoints(planePointsB);
-        // planeGeometryB.computeVertexNormals();
-        //
-        // const material = new THREE.MeshBasicMaterial( {color: red, side: THREE.DoubleSide} );
-        // this.scene.add(new THREE.Mesh( planeGeometryA, material ));
-        // this.scene.add(new THREE.Mesh( planeGeometryB, material ));
-      }
-    }
+    this.scene.add(line);
+  }
 
-    // Render points
-    // const points = [].concat.apply([], xPoints);
-    // const geometry = new THREE.BufferGeometry().setFromPoints(points);
-    // const material = new THREE.PointCloudMaterial({color: white, size: 0.25});
-    // const expressionPoints = new THREE.Points(geometry, material);
-    // this.scene.add(expressionPoints);
+  renderExpressionPlane(expPoints, x, y){
+    let planePointsA = [expPoints[x][y], expPoints[x+1][y], expPoints[x+1][y+1]];
+    let planeGeometryA = new THREE.BufferGeometry().setFromPoints(planePointsA);
+    planeGeometryA.computeVertexNormals();
+
+    let planePointsB = [expPoints[x][y], expPoints[x][y+1], expPoints[x+1][y+1]];
+    let planeGeometryB = new THREE.BufferGeometry().setFromPoints(planePointsB);
+    planeGeometryB.computeVertexNormals();
+
+    const material = new THREE.MeshBasicMaterial( {color: red, side: THREE.DoubleSide} );
+    this.scene.add(new THREE.Mesh( planeGeometryA, material ));
+    this.scene.add(new THREE.Mesh( planeGeometryB, material ));
   }
 }
