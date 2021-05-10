97b34932925ba9d20c8f30ade3b6bdab0cc62507
James <conwayjw97@gmail.com>
2021-05-10 22:42:43 +0100

    Begun work on range inputs but not finished validation

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 
 function App() {
   const [updateCount, setUpdateCount] = useState(0);
-  const [detail, setDetail] = useState(100);
+  const [detail, setDetail] = useState(50);
   const [expression, setExpression] = useState("1-abs(x+y)-abs(y-x)");
+  const [xRange, setXRange] = useState([-100, 100]);
+  const [yRange, setYRange] = useState([-100, 100]);
 
   const handleDetailChange = (event) => {
     setDetail(event.target.value);
@@ -14,6 +16,23 @@ function App() {
 
   const handleExpressionChange = (event) => {
     setExpression(event.target.value);
+  }
+
+  const handleXrangeChange = (event) => {
+    const range = event.target.value;
+    console.log(range);
+    if(!isNaN(parseInt(range[0])) && !isNaN(parseInt(range[1])) && parseInt(range[0])<parseInt(range[1])){
+      console.log([range[0], range[1]]);
+      console.log([parseInt(range[0]), parseInt(range[1])]);
+      setXRange([parseInt(range[0]), parseInt(range[1])]);
+    }
+  }
+
+  const handleYrangeChange = (event) => {
+    const range = event.target.value.split(",");
+    if(range.length=2 && !isNaN(range[0]) && !isNaN(range[1]) && range[0]<range[1]){
+      setYRange([parseInt(range[0]), parseInt(range[1])]);
+    }
   }
 
   const handleUpdate = () => {
@@ -32,6 +51,10 @@ function App() {
         handleDetailChange={handleDetailChange}
         expression={expression}
         handleExpressionChange={handleExpressionChange}
+        xRange={xRange}
+        handleXrangeChange={handleXrangeChange}
+        yRange={yRange}
+        handleYrangeChange={handleYrangeChange}
         handleUpdate={handleUpdate}
       />
     </div>
--- src/components/controls/Controls.js
+++ src/components/controls/Controls.js
@@ -7,9 +7,13 @@ function Controls(props) {
       <div className="expression">
         <label>Detail: </label>
         <input type="range" id="detail" name="detail" min="10" max="200" value={props.detail} onChange={props.handleDetailChange} />
-        <label>Z = </label>
+        <label> Z = </label>
         <input type="text" id="expression" name="expression" value={props.expression} onChange={props.handleExpressionChange} />
         <button onClick={props.handleUpdate}>Update</button>
+        <label> X Range: </label>
+        <input type="text" id="xRange" name="xRange" value={props.xRange} onChange={props.handleXrangeChange} style={{width: "5em"}} />
+        <label>Y Range: </label>
+        <input type="text" id="yRange" name="yRange" value={props.yRange} onChange={props.handleYrangeChange} style={{width: "5em"}} />
       </div>
     </div>
   );
