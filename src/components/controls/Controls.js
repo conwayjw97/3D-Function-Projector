86ab74cc9ca86de19e2575bdbb334d98ae393892
James <conwayjw97@gmail.com>
2021-05-13 10:51:47 +0100

    First version of burger menu working

--- src/components/controls/Controls.js
+++ src/components/controls/Controls.js
@@ -16,47 +16,56 @@ function Controls(props) {
       <label className="centered large-print underlined" style={{padding: "1em 0"}}>
           <button onClick={props.handleUpdate}>Update</button>
       </label>
+
+      <label className="centered large-print">
+          Axis Ranges
+      </label>
+      <label className="centered">
+        X Range: [
+        <input type="text" id="lowerXRange" value={props.ranges[0][0]} onChange={props.handleRangeChange} style={{width: "2.5em"}} />
+        ,
+        <input type="text" id="upperXRange" value={props.ranges[0][1]} onChange={props.handleRangeChange} style={{width: "2.5em"}} />
+        ]
+      </label>
+      <label className="centered">
+        Y Range: [
+        <input type="text" id="lowerYRange" value={props.ranges[1][0]} onChange={props.handleRangeChange} style={{width: "2.5em"}} />
+        ,
+        <input type="text" id="upperYRange" value={props.ranges[1][1]} onChange={props.handleRangeChange} style={{width: "2.5em"}} />
+        ]
+      </label>
+      <label className="centered underlined" style={{padding: "1em 0"}}>
+        Z Range: [
+        <input type="text" id="lowerZRange" value={props.ranges[2][0]} onChange={props.handleRangeChange} style={{width: "2.5em"}} />
+        ,
+        <input type="text" id="upperZRange" value={props.ranges[2][1]} onChange={props.handleRangeChange} style={{width: "2.5em"}} />
+        ]
+      </label>
+
+      <label className="centered large-print">
+          Detail
+      </label>
+      <label className="centered underlined" style={{padding: "1em 0"}}>
+        <input type="range" id="detail" name="detail" min="10" max="100" value={props.detail} onChange={props.handleDetailChange} />
+      </label>
+
+      <label className="centered large-print">
+          Render Features
+      </label>
+      <label className="centered">
+        Points:
+        <input id="pointsToggle" type="checkbox" defaultChecked onChange={props.handleRenderingFeatureChange} />
+      </label>
+      <label className="centered">
+        Squares:
+        <input id="squaresToggle" type="checkbox" defaultChecked onChange={props.handleRenderingFeatureChange} />
+      </label>
+      <label className="centered underlined" style={{padding: "1em 0"}}>
+        Planes:
+        <input id="planesToggle" type="checkbox" defaultChecked onChange={props.handleRenderingFeatureChange} />
+      </label>
     </Menu>
   );
 }
 
 export default Controls;
-
-
-// <div className="controls">
-//   <div className="expression">
-//     <label>Detail: </label>
-//     <input type="range" id="detail" name="detail" min="10" max="100" value={props.detail} onChange={props.handleDetailChange} />
-//
-//     <label> Z = </label>
-//     <input type="text" id="expression" name="expression" value={props.expression} onChange={props.handleExpressionChange} />
-//     <button onClick={props.handleUpdate}>Update</button>
-//
-//     <label> X Range: [</label>
-//     <input type="text" id="lowerXRange" value={props.ranges[0][0]} onChange={props.handleRangeChange} style={{width: "2.5em"}} />
-//     <label>,</label>
-//     <input type="text" id="upperXRange" value={props.ranges[0][1]} onChange={props.handleRangeChange} style={{width: "2.5em"}} />
-//     <label>]</label>
-//
-//     <label> Y Range: [</label>
-//     <input type="text" id="lowerYRange" value={props.ranges[1][0]} onChange={props.handleRangeChange} style={{width: "2.5em"}} />
-//     <label>,</label>
-//     <input type="text" id="upperYRange" value={props.ranges[1][1]} onChange={props.handleRangeChange} style={{width: "2.5em"}} />
-//     <label>]</label>
-//
-//     <label> Z Range: [</label>
-//     <input type="text" id="lowerZRange" value={props.ranges[2][0]} onChange={props.handleRangeChange} style={{width: "2.5em"}} />
-//     <label>,</label>
-//     <input type="text" id="upperZRange" value={props.ranges[2][1]} onChange={props.handleRangeChange} style={{width: "2.5em"}} />
-//     <label>]</label>
-//
-//     <label> Points: </label>
-//     <input id="pointsToggle" type="checkbox" defaultChecked onChange={props.handleRenderingFeatureChange} />
-//
-//     <label> Squares: </label>
-//     <input id="squaresToggle" type="checkbox" defaultChecked onChange={props.handleRenderingFeatureChange} />
-//
-//     <label> Planes: </label>
-//     <input id="planesToggle" type="checkbox" defaultChecked onChange={props.handleRenderingFeatureChange} />
-//   </div>
-// </div>
